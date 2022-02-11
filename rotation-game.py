27270009675d9e77fb593d01fc4b2e5d917308eb
Jeremy Clark <jeremyclark456@gmail.com>
2022-02-10 19:26:24 -0800

    Wrote some stuff.

--- rotation-game.py
+++ rotation-game.py
@@ -11,7 +11,8 @@ pygame.init()
 # Set up the drawing window
 screen = pygame.display.set_mode([500, 500])
 
-origin = np.ndarray([0, 0, 0], np.int32)
+origin = np.zeros((3,), np.float32)
+origin = np.ndarray([0, 0, 0], np.float32)
 
 def degrees_to_radians(degrees):
     return degrees * math.pi / 180
@@ -19,45 +20,53 @@ def degrees_to_radians(degrees):
 def rotate(point, degrees, axis):
     radians = degrees_to_radians(degrees)
     axis_dict = {
-        'x': np.ndarray([[1, 0,                 0                 ],
+        'x': np.array([[1, 0,                 0                 ],
                          [0, math.cos(radians), -math.sin(radians)],
                          [0, math.sin(radians), math.cos(radians)]],
-                         np.int32),
-        'y': np.ndarray([[math.cos(radians), 0, -math.sin(radians)],
+                         np.float32),
+        'y': np.array([[math.cos(radians), 0, -math.sin(radians)],
                          [0,                 1, 0                 ],
                          [math.sin(radians), 0, math.cos(radians)]],
-                         np.int32),
-        'z': np.ndarray([[math.cos(radians), -math.sin(radians), 0],
+                         np.float32),
+        'z': np.array([[math.cos(radians), -math.sin(radians), 0],
                          [math.sin(radians), math.cos(radians),  0],
                          [0,                 0,                  1]],
-                         np.int32)
+                         np.float32)
     }
     rotation_matrix = axis_dict[axis]
     return np.dot(point, rotation_matrix)
     
 def get_distance(point1, point2):
-    return float(abs(np.linalg.norm(point1-point2)))
+    return np.abs(np.linalg.norm(point1-point2))
 
-def get_skewed_point(axis, camera_coords, angle):
+def get_skewed_axis(axis, camera_coords, angle):
+    # 0 = x, 1 = y, 2 = z
     if not -90 < angle < 90:
         raise AssertionError("Angle must be between -90 and 90 degrees.")
     theta = degrees_to_radians(angle)
-    axis_dict = {'x': 0, 'y': 1, 'z': 2}
-    axis_index = axis_dict[axis]
-    D = np.zeros((3))
-    j = camera_coords[axis_index]
-    D[axis_index] = j
+    j = camera_coords[axis]
     i = get_distance(camera_coords, origin)
     phi = math.arcsin(j/i)
-    mu = 180 - phi
-    psi = 180 - mu - theta
+    mu = 2 * math.pi - phi
+    psi = 2 * math.pi - mu - theta
     m = i * math.sin(theta) / math.sin(psi)
-    
+    return m
 
-def make_limit_funcs(camera_orientation, camera_coords, horz_angle, vert_angle):
-    x, y, z = camera_coords
-    run = get_distance(camera_coords, origin)
+def get_slope(camera_coords, orientation_vec):
+    skewed_point = np.array([get_skewed_axis(i, camera_coords, orientation_vec[i]) for i in range(3)], np.float32)
+    run = get_distance(skewed_point)
     camera_slope = camera_coords / run
+    return camera_slope
+
+def get_line_equation(camera_coords, orientation_vec):
+    camera_slope = get_slope(camera_coords, orientation_vec)
+    def eqn(x):
+        return x * camera_slope + camera_coords
+    return eqn
+
+def get_skewed_orientation(horz_skew, vert_skew, orienation_vec, quadrant):
+    # Top left = 1, top right = 2, bottom right = 3, bottom left = 4
+    
 
 # Run until the user asks to quit
 running = True
